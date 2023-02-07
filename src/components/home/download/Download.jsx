fe990645c115df1ef555021d4b6ef1e47870848d
joaquin grosso <joagrosso@hotmail.com>
2023-02-07 14:43:41 -0300

    recuperar canciones de una playlist listo

--- src/components/home/download/Download.jsx
+++ src/components/home/download/Download.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect , useRef} from 'react'
+import playlistToTxt from '../../api/playlist'
 import Nav from '../../nav/Nav'
 
+
 const Download = () => {
+  
+  const handleClick = (e) => {
+    e.preventDefault()
+
+    const playlistLink = e.target.inputPlaylist.value;
+
+    playlistToTxt( playlistLink );
+
+  }
+
   return (
     <>
     <Nav/>
@@ -9,10 +21,10 @@ const Download = () => {
       <h2>Your PlayList <span>/</span>  song from <strong>Spotify</strong> </h2>
       <p>Paste the URL of your Spotify PlayList / song and start your download</p>
     
-      <form className="box_contact_main">
-        <input type="text" required placeholder='url' />
+      <form className="box_contact_main" onSubmit={(handleClick)}>
+        <input id='playlistId' name='inputPlaylist'  type="text" required placeholder='url' />
       
-        <button className="sendContact_main" type='submit'>
+        <button className="sendContact_main" type='submit' >
             Send
         </button>
       </form>
