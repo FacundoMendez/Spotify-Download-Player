864630c00983090050683f802d1b60393160e78a
facuMendez <facundo.mendez@live.com.ar>
2023-02-24 15:14:28 -0300

    download icon

--- client/src/components/home/download/Download.jsx
+++ client/src/components/home/download/Download.jsx
@@ -15,7 +15,7 @@ const Download = () => {
 
   /* loader download*/
 
-  const [loader, setLoader] = useState(false)
+  const [loader, setLoader] = useState()
 
 
   /* acces token dinamico url */
@@ -36,6 +36,8 @@ const Download = () => {
 
     /* funcion que llama a playList.js y envia los parametros (access Token y la playlist que se le envia por input) */
 
+    const [downloadActive , setDownloadActive] = useState(false)
+
   const handleClick = (e) => {
       e.preventDefault()
 
@@ -43,13 +45,16 @@ const Download = () => {
       //spotifyClient( playlistLink , UserAccessToken);
       
       //downloadPlaylist(playlistLink, UserAccessToken);
+
       setLoader(true)
+
       axios.post("http://localhost:3000/playlist",{ 
           link: playlistLink, 
           token: UserAccessToken
         })
         .then(function (response) {
           setLoader(false)
+          setDownloadActive(true)
           console.log(response.data);
         })
   }
@@ -92,9 +97,11 @@ const Download = () => {
 
     /* validacion de input */
 
+  const [inputVacio , setInputVacio] = useState(true)
+
   function validarInput() {
-      const input = document.getElementById('playlistId');
-      const valorInput = input.value;
+    const input = document.getElementById('playlistId');
+    const valorInput = input.value;
 
       // Verificar si el valor del input es válido
       if (valorInput.trim() === '') {
@@ -104,7 +111,11 @@ const Download = () => {
           setError("")
         }, 4300);
 
+        setInputVacio(true)
+
         return false;
+      }else{
+        setInputVacio(false)
       }
 
       // Si el valor del input es válido, ejecutar la función descargarArchivo()
@@ -140,7 +151,7 @@ const Download = () => {
     
         <div className="boxForm">
           <form className="box_contact_main" onSubmit={(handleClick)}>
-              <input id='playlistId' name='inputPlaylist'  type="text" required placeholder='url' />
+              <input id='playlistId' name='inputPlaylist'  type="text"  placeholder='url' />
             
               <select name="typeSong" id="typeSong" value={typeSong} onChange={handleChange}>
                 <option className='option' value="mp3">.mp3</option>
@@ -148,8 +159,14 @@ const Download = () => {
               </select>
 
               {
-                loader ? 
+                loader && !inputVacio ? 
                   <div className=" sendContact_main-loader" > <Load/> </div>
+                :
+                downloadActive ? 
+
+                  <button className="sendContact_main"  > 
+                    <svg fill="#ffffff" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>download</title> <path d="M18.313 13.625h-4.031v-6.594c0-0.563-0.469-1.031-1.031-1.031h-4.031c-0.594 0-1.063 0.469-1.063 1.031v6.594h-4.031c-0.531 0-0.719 0.344-0.313 0.75l6.688 6.656c0.188 0.188 0.438 0.281 0.719 0.281s0.563-0.094 0.75-0.281l6.656-6.656c0.375-0.406 0.25-0.75-0.313-0.75zM0 18.344v7.125c0 0.313 0.156 0.5 0.5 0.5h21.375c0.344 0 0.531-0.188 0.531-0.5v-7.125c0-0.313-0.25-0.531-0.531-0.531h-2.031c-0.281 0-0.531 0.25-0.531 0.531v4.531h-16.25v-4.531c0-0.313-0.219-0.531-0.5-0.531h-2.063c-0.281 0-0.5 0.25-0.5 0.531z"></path> </g></svg>
+                  </button>
                 :
                   <button className="sendContact_main" onClick={validarInput} type="submit" > Send</button>
               }
